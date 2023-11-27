081b254d593cbac863f9b5d5e55c3465bae2a224
jeng-yuantsai <tsaie79@gmail.com>
2023-11-27 01:19:38 +0000

    update

--- sf-api/example.py
+++ sf-api/example.py
@@ -1,5 +1,8 @@
 from authlib.integrations.requests_client import OAuth2Session
 from authlib.oauth2.rfc7523 import PrivateKeyJWT
+from pprint import pprint
+from time import sleep
+
 
 token_url = "https://oidc.nersc.gov/c2id/token"
 client_id = "eu2ajfe5zyqfs"
@@ -16,27 +19,68 @@ session.fetch_token()
 
 
 def check_systems():
-    system = "perlmutter"
     verb = "status"
+    system = "perlmutter"
     url = f"https://api.nersc.gov/api/v1.2/{verb}/{system}"
     resp = session.get(url)
     json = resp.json()
     # indent=2 for pretty print of json response 
-    print(json)
-
+    pprint(json, indent=2)
 
 def list_directories():
+    verb = "utilities/ls"    
     system = "perlmutter"
-    dir = "/global/homes/j/jlabtsai"
-    verb = "utilities/ls"
+    dir = "/global/homes/j/jlabtsai/swif"
     url = f"https://api.nersc.gov/api/v1.2/{verb}/{system}/{dir}"
     resp = session.get(url)
     json = resp.json()
     # indent=2 for pretty print of json response 
-    print(json)
+    pprint(json, indent=2)
+
+def run_cmd():
+    verb = "utilities/command"
+    system = "perlmutter"
+    url = f"https://api.nersc.gov/api/v1.2/{verb}/{system}"
+
+    cmd = "cat /global/homes/j/jlabtsai/swif/cmd.sh"
+    resp = session.post(url, data={"executable": cmd})
+    json = resp.json()
+    # indent=2 for pretty print of json response 
+    pprint(json, indent=2)
+
+    sleep(10)
+    task_id = json["task_id"]
+    read_result(task_id)
+    
+
+
+def read_result(task_id):
+    # There is a delay between when the task is submitted and when the result is available
+    url = f"https://api.nersc.gov/api/v1.2/tasks/{task_id}"
+    resp = session.get(url)
+    json = resp.json()
+    pprint(json, indent=2)
+
+
+def submit_slurm_job():
+    verb = "compute/jobs"
+    system = "perlmutter"
+    url = f"https://api.nersc.gov/api/v1.2/{verb}/{system}"
+
+    batch_job_script = "/global/homes/j/jlabtsai/run-vk/slurm/ersap1.slurm"
+    resp = session.post(url, data={"job": batch_job_script, "isPath": True})
+    json = resp.json()
+    # indent=2 for pretty print of json response 
+    pprint(json, indent=2)
+
+    sleep(10)
+    task_id = json["task_id"]
+    read_result(task_id)
 
 
 if __name__ == "__main__":
-    check_systems()
-    list_directories()
+    # check_systems()
+    # list_directories()
+    # run_cmd()
+    submit_slurm_job()
 
